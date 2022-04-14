f0c2efdf43adce09cc8e513ca69ee2566ed688be
Til121 <103142294+Til121@users.noreply.github.com>
2022-04-14 17:04:47 +0200

    Update Main.java

--- JsonReader/src/Main.java
+++ JsonReader/src/Main.java
@@ -8,26 +8,26 @@ import java.sql.Timestamp;
 public class Main {
 
     public static void main(String[] args) {
+        WriteHtml Write = new WriteHtml("./test.html");
+        Write.HtmlStart();
+        Write.Head("Branje Json");
         try {
             JsonNode json = JsonLoader.fromPath("./Vhod.json");
             for (int index = 0; index < json.get("sporocila").size(); index++) {
-                        // System.out.println(json.get("sporocila").get(index));
+                // System.out.println(json.get("sporocila").get(index));
                 //timestamp = number of seconds from 1970.01.01 00:00
                 //group objects by IP address
 
-                WriteHtml Write = new WriteHtml("./test.html");
-
-                Write.HtmlStart();
-                Write.Head("Branje Json");
-
                 Timestamp ts = new Timestamp(json.get("sporocila").get(index).get("timestamp").asLong());
-
-                Write.TableRow(ts);
-                Write.HtmlEnd();
-
+                String address = json.get("sporocila").get(index).get("address").asText();
+                String message = json.get("sporocila").get(index).get("sporocilo").asText();
+                Write.TableRow(ts, address, message);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
+
+        Write.HtmlEnd();
     }
 }
